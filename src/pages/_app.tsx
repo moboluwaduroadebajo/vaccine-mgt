229b6e61bee3d69bece3d786b72bb97d44277d46
Boluwaduro00 <adebajobolu@gmail.com>
2024-07-08 22:23:26 +0100

    optimize data fetching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,17 @@ import { league_spartan, poppins } from "@/utils/font";
 import type { AppProps } from "next/app";
 import { store } from "@/app/store";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
   return (
-    <Provider store={store}>
-      <main className={`${league_spartan} ${poppins}`}>
-        <Component {...pageProps} />
-      </main>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <main className={`${league_spartan} ${poppins}`}>
+          <Component {...pageProps} />
+        </main>
+      </Provider>
+    </QueryClientProvider>
   );
 }
-
